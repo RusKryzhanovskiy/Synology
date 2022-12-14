94c0667ceb39ed2ec9f81ac285b05488e6ac85cb
Ruslan Kryzhanovskyi <ruskryzhanovskyi@gmail.com>
2022-12-14 15:48:36 +0100

    Refactoring

--- lib/core/network/services/impl/dio/isolated_callback.dart
+++ lib/core/network/services/impl/dio/isolated_callback.dart
@@ -3,6 +3,7 @@ part of 'dio_network_service.dart';
 NetworkResponse<T> _isolatedCallback<T>(_JsonParserRequest<T> request) {
   try {
     final data = request.json;
+
     if (data is Map<String, dynamic>) {
       return NetworkResponse.success(request.jsonParser(data));
     }
@@ -10,8 +11,8 @@ NetworkResponse<T> _isolatedCallback<T>(_JsonParserRequest<T> request) {
     return const NetworkResponse.error();
   } on DioError catch (error) {
     return NetworkResponse.error(
-      statusCode: error.response?.statusCode,
       message: error.message,
+      statusCode: error.response?.statusCode,
     );
   } catch (error) {
     return NetworkResponse.error(message: error.toString());
